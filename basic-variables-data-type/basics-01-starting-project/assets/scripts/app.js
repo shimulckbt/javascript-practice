60ec95755d639d4ea48efa4956bd980ea6191fcb
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-15 16:30:16 +0600

    adding error solved

--- basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
+++ basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
@@ -3,7 +3,7 @@ let currentResult = defaultResult;
 
 
 function add() {
-   currentResult = currentResult + userInput.value;
+   currentResult = currentResult + parseInt(userInput.value);
    outputResult(currentResult, '');
 }
 
