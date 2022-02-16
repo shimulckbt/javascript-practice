888b3434917bd38052d00ee4253ec70471fc83cb
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-16 14:06:15 +0600

    little modification in addition

--- basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
+++ basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumber() {
+   return parseInt(userInput.value);
+}
 
 function add() {
-   currentResult = currentResult + parseInt(userInput.value);
-   outputResult(currentResult, '');
+   const enteredNumber = getUserNumber();
+   const calcDescription = `${currentResult} + ${enteredNumber}`;
+   // currentResult = currentResult + parseInt(enteredNumber);//work as below line but specify only integer
+   currentResult = currentResult + +(enteredNumber);
+   outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
