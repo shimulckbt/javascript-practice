570d0e4260d952c75d0eb4bf8428e1a57a819c6b
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-16 14:11:07 +0600

    simple calcultaor

--- basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
+++ basic-variables-data-type/basics-01-starting-project/assets/scripts/app.js
@@ -13,4 +13,31 @@ function add() {
    outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+   const enteredNumber = getUserNumber();
+   const calcDescription = `${currentResult} - ${enteredNumber}`;
+   // currentResult = currentResult + parseInt(enteredNumber);//work as below line but specify only integer
+   currentResult = currentResult - +(enteredNumber);
+   outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+   const enteredNumber = getUserNumber();
+   const calcDescription = `${currentResult} * ${enteredNumber}`;
+   // currentResult = currentResult + parseInt(enteredNumber);//work as below line but specify only integer
+   currentResult = currentResult * +(enteredNumber);
+   outputResult(currentResult, calcDescription);
+}
+
+function devide() {
+   const enteredNumber = getUserNumber();
+   const calcDescription = `${currentResult} / ${enteredNumber}`;
+   // currentResult = currentResult + parseInt(enteredNumber);//work as below line but specify only integer
+   currentResult = currentResult / +(enteredNumber);
+   outputResult(currentResult, calcDescription);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', devide);
