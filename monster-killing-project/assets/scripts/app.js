68e51b51717bcebd218ff8d9ad472c35410e93e7
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-17 19:00:41 +0600

    monster can hit

--- monster-killing-project/assets/scripts/app.js
+++ monster-killing-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 let chosenMaxLife = 100;
 let currentMonsterLife = chosenMaxLife;
 let currentPlayerLife = chosenMaxLife;
@@ -8,9 +9,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
    const damage = dealMonsterDamage(ATTACK_VALUE);
    currentMonsterLife -= damage;
+   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+   currentPlayerLife -= playerDamage;
    if (currentMonsterLife <= 0) {
       alert('You Won');
+   } else if (currentPlayerLife <= 0) {
+      alert('You Lost');
    }
+
+
 }
 
 attackBtn.addEventListener('click', attackHandler);
