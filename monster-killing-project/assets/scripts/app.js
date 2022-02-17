e6c2723df666d1174b295e8757c2f0e46d2f5973
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-17 19:06:23 +0600

    match draw

--- monster-killing-project/assets/scripts/app.js
+++ monster-killing-project/assets/scripts/app.js
@@ -1,6 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
-let chosenMaxLife = 100;
+let chosenMaxLife = 5;
 let currentMonsterLife = chosenMaxLife;
 let currentPlayerLife = chosenMaxLife;
 
@@ -11,10 +11,12 @@ function attackHandler() {
    currentMonsterLife -= damage;
    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
    currentPlayerLife -= playerDamage;
-   if (currentMonsterLife <= 0) {
+   if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
       alert('You Won');
-   } else if (currentPlayerLife <= 0) {
+   } else if (currentPlayerLife <= 0 && currentMonsterLife > 0) {
       alert('You Lost');
+   } else if (currentPlayerLife <= 0 && currentMonsterLife <= 0) {
+      alert(`It's a draw`);
    }
 
 
