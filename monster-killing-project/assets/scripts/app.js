e0db4a4d7bebe73f6da3fd66cce3b56431ee1153
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-03 17:55:04 +0600

    strong attack

--- monster-killing-project/assets/scripts/app.js
+++ monster-killing-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 let chosenMaxLife = 5;
 let currentMonsterLife = chosenMaxLife;
 let currentPlayerLife = chosenMaxLife;
@@ -19,4 +20,19 @@ function attackHandler() {
       alert(`It's a draw`);
    }
 }
+
+function strongAttackHandler() {
+   const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+   currentMonsterLife -= damage;
+   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+   currentPlayerLife -= playerDamage;
+   if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
+      alert('You Won');
+   } else if (currentPlayerLife <= 0 && currentMonsterLife > 0) {
+      alert('You Lost');
+   } else if (currentPlayerLife <= 0 && currentMonsterLife <= 0) {
+      alert(`It's a draw`);
+   }
+}
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
