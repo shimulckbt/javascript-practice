7736dae59e5a2c347f7da386ac44b6d9144415a3
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-10 23:39:50 +0600

    modification

--- monster-killing-project/assets/scripts/app.js
+++ monster-killing-project/assets/scripts/app.js
@@ -145,3 +145,23 @@ attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler)
 healBtn.addEventListener('click', healPlayerHandler)
 logBtn.addEventListener('click', printLogHandler);
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
+logBtn.addEventListener('click', printLogHandler);
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
+logBtn.addEventListener('click', printLogHandler);
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
+logBtn.addEventListener('click', printLogHandler);
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
+logBtn.addEventListener('click', printLogHandler);
