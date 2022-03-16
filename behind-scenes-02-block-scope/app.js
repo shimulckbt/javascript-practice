298bac2259bbad665adff5c88812ae94af2a67c5
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-17 00:03:53 +0600

    var vs let scope

---
+++ behind-scenes-02-block-scope/app.js
@@ -0,0 +1,17 @@
+let name = 'Shimul';
+
+if (name === 'Shimul') {
+  let hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
