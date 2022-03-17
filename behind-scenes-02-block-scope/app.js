5d185ef2d3ae4a79bb70e0068e39202cbe97f7a4
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-17 13:02:00 +0600

    modification

--- behind-scenes-02-block-scope/app.js
+++ behind-scenes-02-block-scope/app.js
@@ -1,17 +1,23 @@
-let name = 'Shimul';
+// let name = 'Shimul';
 
-if (name === 'Shimul') {
-  let hobbies = ['Sports', 'Cooking'];
-  console.log(hobbies);
+// if (name === 'Shimul') {
+//   let hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
 
-}
+// }
 
-function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+
+'use strict';
+
+var userName = 'Shimul';
+
+console.log(userName);
