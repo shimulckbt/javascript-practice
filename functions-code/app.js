e53f577a2ca0805f4fd64e36bb26eb1109f6beb8
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-17 13:00:39 +0600

    winning logic

--- functions-code/app.js
+++ functions-code/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = 'ROCK';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WIN = 'PLAYER_WINS';
+const RESULT_COMPUTER_WIN = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -19,12 +22,34 @@ const getPlayerChoice = function () {
    return selection;
 }
 
+const getComputerChoice = function () {
+   const randomValue = Math.random();
+   if (randomValue > 0.34) {
+      return ROCK;
+   } else if (randomValue > 0.67) {
+      return PAPER;
+   } else {
+      return SCISSORS;
+   }
+}
+
+const getWinner = function (cChoice, pChoice) {
+   if (cChoice === pChoice) {
+      return RESULT_DRAW;
+   } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK) {
+      return RESULT_PLAYER_WIN;
+   } else {
+      return RESULT_COMPUTER_WIN;
+   }
+}
+
 startGameBtn.addEventListener('click', function () {
    if (gameIsRunning) {
       return;
    }
    gameIsRunning = true;
-   console.log('Game is starting..');
    const playerSelection = getPlayerChoice();
-   console.log(playerSelection);
+   const computerSelection = getComputerChoice();
+   const winner = getWinner(computerSelection, playerSelection);
+   console.log(winner);
 })
