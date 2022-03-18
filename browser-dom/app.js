86a03d82d164bb2f7d445450f9e228a944ae4209
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-18 15:06:15 +0600

    change css class

--- browser-dom/app.js
+++ browser-dom/app.js
@@ -9,3 +9,22 @@ liLast.textContent = liLast.textContent + ' (last changed!)';
 const liFirst = document.querySelector('li:first-of-type');
 liFirst.textContent = liFirst.textContent + ' (first changed!)';
 
+ulID = document.querySelector("#asd");
+ulID.style.color = "blue";
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+// section.style.backgroundColor = 'green';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+   // if (section.className === 'red-bg visible') {
+   //    section.className = 'red-bg invisible';
+   // } else {
+   //    section.className = 'red-bg visible';
+   // }
+   // section.classList.toggle('visible');
+   section.classList.toggle('invisible');
+
+})
+
