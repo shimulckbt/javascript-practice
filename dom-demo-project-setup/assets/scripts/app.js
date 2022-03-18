8eac582d166085c6e173c48109a09f073eee8b60
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-18 18:02:48 +0600

    clearing user inputs

--- dom-demo-project-setup/assets/scripts/app.js
+++ dom-demo-project-setup/assets/scripts/app.js
@@ -4,9 +4,15 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const movies = [];
 
 const toggleBackdrop = () => {
    backdrop.classList.toggle('visible');
+}
+const clearUserInputs = () => {
+   for (const userInput of userInputs) {
+      userInput.value = '';
+   }
 }
 
 const toggleMovieModal = () => {
@@ -27,10 +33,21 @@ const addMovieHandler = () => {
       alert('Please enter valid values (rating value should in range of 1 to 5');
       return;
    }
+
+   const newMovie = {
+      title: titleValue,
+      image: imageUrlValue,
+      rating: ratingValue,
+   };
+   movies.push(newMovie);
+   console.log(movies);
+   toggleMovieModal();
+   clearUserInputs();
 }
 
 const cancelAddMovie = () => {
    toggleMovieModal();
+   clearUserInputs();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
