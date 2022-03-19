8ab1c222dadc4bc2e7a554305f87d9207de036e1
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-19 19:55:06 +0600

    WeakSet() WeakMap()

--- arrays-practice/sets-maps.js
+++ arrays-practice/sets-maps.js
@@ -9,14 +9,24 @@ const personData = new Map([[person1, [{ date: 'Today', price: 1000 }]]]);
 personData.set(person2, [{ date: 'Today', price: 10 }]);
 
 for (const [key, value] of personData.entries()) {
-   console.log(key, value);
+   // console.log(key, value);
 }
 
 for (const key of personData.keys()) {
-   console.log(key);
+   // console.log(key);
 }
 
 for (const value of personData.values()) {
-   console.log(value);
+   // console.log(value);
 }
-console.log(personData.size);
+// console.log(personData.size);
+
+let person = { name: 'shimul' };
+const persons = new WeakSet();
+
+// person = null;
+persons.add(person);
+
+console.log(persons);
+
+//WeakMap() works as same logic, it dont stop garbage collector of browser to remove unused data
