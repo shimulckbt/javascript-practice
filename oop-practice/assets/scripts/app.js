edff23abec07ee05e6f2c7a41515776e444bd6d2
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-20 03:58:01 +0600

    cart class visible

--- oop-practice/assets/scripts/app.js
+++ oop-practice/assets/scripts/app.js
@@ -12,6 +12,21 @@ class Product {
    }
 }
 
+class ShoppingCart {
+   items = [];
+
+   render() {
+      const cartEl = document.createElement('section');
+      cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+      cartEl.className = 'cart';
+      // this.totalOutput = cartEl.querySelector('h2');
+      return cartEl;
+   }
+}
+
 class ProductItem {
    constructor(product) {
       this.product = product;
@@ -61,7 +76,6 @@ class ProductList {
    constructor() { }
 
    render() {
-      const renderHook = document.getElementById('app');
       const prodList = document.createElement('ul');
       prodList.className = 'product-list';
       for (const prod of this.products) {
@@ -69,9 +83,25 @@ class ProductList {
          const prodEl = productItem.render();
          prodList.append(prodEl);
       }
-      renderHook.append(prodList);
+      return prodList;
    }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+   render() {
+
+      const renderHook = document.getElementById('app');
+
+      const cart = new ShoppingCart();
+      const cartEl = cart.render();
+      const productList = new ProductList();
+      const prodListEl = productList.render();
+
+      renderHook.append(cartEl);
+      renderHook.append(prodListEl);
+
+   }
+}
+
+const shop = new Shop();
+shop.render();
