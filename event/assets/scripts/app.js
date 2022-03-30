02194e9e449a9c1f48fbcfe69a0f7638b4b9d236
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-03-30 23:21:40 +0600

    component class added

--- event/assets/scripts/app.js
+++ event/assets/scripts/app.js
@@ -13,7 +13,30 @@ class DOMHelper {
   }
 }
 
+class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
 
+  detach() {
+    if (this.element) {
+      this.element.remove();
+      // this.element.parentElement.removeChild(this.element);
+    }
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
 
 class Tooltip extends Component {
   constructor(closeNotifierFunction, text, hostElementId) {
