b55202523fc854f4a7511667e4f16dece1f9914c
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-02 18:32:14 +0600

    some promise method

--- async-javascript/app.js
+++ async-javascript/app.js
@@ -62,6 +62,18 @@ async function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
+// Promise.race([getPosition(), setTimer(1000)]).then(data => {
+//   console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData);
+// });
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+});
+
 // let result = 0;
 
 // for (let i = 0; i < 100000000; i++) {
