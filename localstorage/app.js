3f8a1c025fdf15a54e30d0be252c24aa466e6243
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-03 01:26:10 +0600

    basic cookie

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,25 +1,11 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-const userId = 'u123';
-const user = {
-  name: 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking']
-};
-
 storeBtn.addEventListener('click', () => {
-  sessionStorage.setItem('uid', userId);
-  localStorage.setItem('user', JSON.stringify(user));
+  const userId = 'u123';
+  document.cookie = `uid=${userId}`;
 });
 
 retrBtn.addEventListener('click', () => {
-  const extractedId = sessionStorage.getItem('uid');
-  const extractedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(extractedUser);
-  if (extractedId) {
-    console.log('Got the id - ' + extractedId);
-  } else {
-    console.log('Could not find id.');
-  }
+  console.log(document.cookie);
 });
