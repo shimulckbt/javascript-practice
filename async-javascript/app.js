e4ffa9f5efc5d776995d178bf6e51f86ca9c210a
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-02 17:40:07 +0600

    promise chaining

--- async-javascript/app.js
+++ async-javascript/app.js
@@ -1,6 +1,17 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(success => {
+      resolve(success);
+    }, error => {
+
+    }, opts);
+  });
+  return promise;
+}
+
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -11,19 +22,22 @@ const setTimer = (duration) => {
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimer(2000).then(data => {
-        console.log(data, posData);
-      });
-    },
-    error => {
-      console.log(error);
-    }
-  );
-  setTimeout(() => {
-    console.log('Timer done!');
-  }, 0);
+  let positionData;
+  getPosition()
+    .then(posData => {
+      positionData = posData;
+      console.log(posData);
+      return setTimer(1000);
+    })
+    .then(data => {
+      console.log(data, positionData);
+    });
+
+  setTimer(1000)
+    .then(() => {
+      console.log('Timer Done!');
+    });
+
   console.log('Getting position...');
 }
 
