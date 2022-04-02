6927ed1b68ceec09dde46c66714c6bb10d081096
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-02 17:19:59 +0600

    track location using promise

--- async-javascript/app.js
+++ async-javascript/app.js
@@ -1,12 +1,21 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then(data => {
+        console.log(data, posData);
+      });
     },
     error => {
       console.log(error);
