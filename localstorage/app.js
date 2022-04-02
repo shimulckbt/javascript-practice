b8310717b95ed192e5d41fbd619e4c9a413a0777
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-03 01:56:15 +0600

    indexedDb initial

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,18 +1,24 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
+const dbRequest = indexedDB.open('StorageDummy', 1);
+dbRequest.onSuccess = function (event) {
+  const db = event.target.result;
+
+  const objStore = db.createObjectStore('products', { keyPath: 'id' });
+  objStore.transaction.oncomplete = function (event) {
+    db.transaction('products', 'readonly');
+  };
+}
+
+dbRequest.onError = function (event) {
+  console.log('error');
+}
+
 storeBtn.addEventListener('click', () => {
-  const userId = 'u123';
-  const user = { name: 'shimul', age: 27 };
-  document.cookie = `uid=${userId}; max-age=5`;
-  document.cookie = `user=${JSON.stringify(user)}`;
+
 });
 
 retrBtn.addEventListener('click', () => {
-  const cookieData = document.cookie.split(';');
 
-  const data = cookieData.map(i => {
-    return i.trim();
-  });
-  console.log(data);
 });
---
+++ localstorage/cookies.js
@@ -0,0 +1,18 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+storeBtn.addEventListener('click', () => {
+   const userId = 'u123';
+   const user = { name: 'shimul', age: 27 };
+   document.cookie = `uid=${userId}; max-age=5`;
+   document.cookie = `user=${JSON.stringify(user)}`;
+});
+
+retrBtn.addEventListener('click', () => {
+   const cookieData = document.cookie.split(';');
+
+   const data = cookieData.map(i => {
+      return i.trim();
+   });
+   console.log(data);
+});
