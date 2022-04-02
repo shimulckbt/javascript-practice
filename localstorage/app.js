50d858cd256ffa59bad8f062280d33abdd6810a7
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-03 01:44:47 +0600

    cookie saving and expire time

--- localstorage/app.js
+++ localstorage/app.js
@@ -3,9 +3,16 @@ const retrBtn = document.getElementById('retrieve-btn');
 
 storeBtn.addEventListener('click', () => {
   const userId = 'u123';
-  document.cookie = `uid=${userId}`;
+  const user = { name: 'shimul', age: 27 };
+  document.cookie = `uid=${userId}; max-age=5`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener('click', () => {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(';');
+
+  const data = cookieData.map(i => {
+    return i.trim();
+  });
+  console.log(data);
 });
