5a3cbf062129d9c79d6b24e018f08e4e4ac0c08c
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-03 02:00:48 +0600

    more indexedDb

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,24 +1,58 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
+let db;
+
 const dbRequest = indexedDB.open('StorageDummy', 1);
-dbRequest.onSuccess = function (event) {
-  const db = event.target.result;
+
+dbRequest.onsuccess = function (event) {
+  db = event.target.result;
+};
+
+dbRequest.onupgradeneeded = function (event) {
+  db = event.target.result;
 
   const objStore = db.createObjectStore('products', { keyPath: 'id' });
-  objStore.transaction.oncomplete = function (event) {
-    db.transaction('products', 'readonly');
-  };
-}
 
-dbRequest.onError = function (event) {
-  console.log('error');
-}
+  objStore.transaction.oncomplete = function (event) {
+    const productsStore = db
+      .transaction('products', 'readwrite')
+      .objectStore('products');
+    productsStore.add({
+      id: 'p1',
+      title: 'A First Product',
+      price: 12.99,
+      tags: ['Expensive', 'Luxury']
+    });
+  };
+};
+
+dbRequest.onerror = function (event) {
+  console.log('ERROR!');
+};
 
 storeBtn.addEventListener('click', () => {
-
+  if (!db) {
+    return;
+  }
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  productsStore.add({
+    id: 'p2',
+    title: 'A Second Product',
+    price: 122.99,
+    tags: ['Expensive', 'Luxury']
+  });
 });
 
 retrBtn.addEventListener('click', () => {
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  const request = productsStore.get('p2');
 
+  request.onsuccess = function () {
+    console.log(request.result);
+  }
 });
