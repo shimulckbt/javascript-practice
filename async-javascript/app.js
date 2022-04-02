03ff7175d1c64fdfd6a4937412d187c2b4f13502
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-02 18:15:46 +0600

    promose and error handling with async-await and try-catch

--- async-javascript/app.js
+++ async-javascript/app.js
@@ -6,7 +6,7 @@ const getPosition = (opts) => {
     navigator.geolocation.getCurrentPosition(success => {
       resolve(success);
     }, error => {
-
+      reject(error);
     }, opts);
   });
   return promise;
@@ -20,25 +20,44 @@ const setTimer = (duration) => {
   });
   return promise;
 };
+////////       promise without async wait        //////////
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-    .then(posData => {
-      positionData = posData;
-      console.log(posData);
-      return setTimer(1000);
-    })
-    .then(data => {
-      console.log(data, positionData);
-    });
+// function trackUserHandler() {
+//   let positionData;
+//   getPosition()
+//     .then(posData => {
+//       positionData = posData;
+//       // console.log(posData);
+//       return setTimer(2000);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     })
+//     .then(data => {
+//       console.log(data, positionData);
+//     });
 
-  setTimer(1000)
-    .then(() => {
-      console.log('Timer Done!');
-    });
+//   // setTimer(1000)
+//   //   .then(() => {
+//   //     console.log('Timer Done!');
+//   //   });
 
-  console.log('Getting position...');
+//   console.log('Getting position...');
+// }
+
+//////////     Promise with async await         /////////
+
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
 }
 
 button.addEventListener('click', trackUserHandler);
