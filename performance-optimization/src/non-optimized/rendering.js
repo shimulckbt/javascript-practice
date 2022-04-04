7df5717a6f56a94e48ebb866ed01aaf218103ffb
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-05 04:43:53 +0600

    rendenfile

---
+++ performance-optimization/src/non-optimized/rendering.js
@@ -0,0 +1,17 @@
+export function renderProducts(products, deleteProductFn) {
+  const productListEl = document.getElementById('product-list');
+  productListEl.innerHTML = '';
+  products.forEach(product => {
+    const newListEl = document.createElement('li');
+    const prodTitleEl = document.createElement('h2');
+    const prodPriceEl = document.createElement('p');
+    const prodDeleteButtonEl = document.createElement('button');
+
+
+    newListEl.appendChild(prodTitleEl);
+    newListEl.appendChild(prodPriceEl);
+    newListEl.appendChild(prodDeleteButtonEl);
+
+    productListEl.appendChild(newListEl);
+  });
+}
